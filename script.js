fa468ed55bfb30c53d4d9a535ad89da44ba583a2
Shreya <contact.shreya22@gmail.com>
2020-10-21 19:30:06 -0500

    added fetch and response

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+
+
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json) {
+
+        });
+
+
+    });
+
+
+
+
+
+
+});
